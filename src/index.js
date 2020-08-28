45690ebb6923cffd2a68f5d371e9bcbc7fbda546
Jay Moon <sjaymoon15@gmail.com>
2020-08-28 13:38:40 -0500

    Add Semantic-ui css library.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import Root from './containers/Root';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <BrowserRouter>
