2f9d67b13c3976f7c28f02584beb38a59ee8f8ce
Jay Moon <sjaymoon15@gmail.com>
2020-09-14 14:31:43 -0500

    Update 404 page.

--- src/containers/NotFoundPage.js
+++ src/containers/NotFoundPage.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { Message } from 'semantic-ui-react';
 
-const NotFoundPage = () => {
-  return <div>404 Not Found</div>;
-};
+const NotFoundPage = () => (
+  <Message negative>
+    <Message.Header>404</Message.Header>
+    <p>Sorry, we couldn't find this page.</p>
+  </Message>
+);
 
 export default NotFoundPage;
