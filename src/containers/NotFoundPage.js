8266dbbc4833439f1b5a1ba16b323688c7028671
Jay Moon <sjaymoon15@gmail.com>
2020-09-17 11:43:40 -0500

    Add Back to Home link to 404 page.

--- src/containers/NotFoundPage.js
+++ src/containers/NotFoundPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Message } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import * as routes from '../constants/routes';
 
 const NotFoundPage = () => (
   <Message negative>
     <Message.Header>404</Message.Header>
     <p>Sorry, we couldn't find this page.</p>
+
+    <Link to={routes.HOME}>Back to Home</Link>
   </Message>
 );
 
