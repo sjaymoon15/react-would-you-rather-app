19730dd2da8203d9b29038b8b31d493511235b3c
Jay Moon <sjaymoon15@gmail.com>
2020-09-30 16:37:02 -0500

    Add questions reducer test.

---
+++ src/reducers/questions.test.js
@@ -0,0 +1,45 @@
+import reducer from './questions';
+import {
+  RECEIVE_QUESTIONS,
+  SAVE_QUESTION_ANSWER_QUESTIONS,
+} from '../actions/types';
+import { questions } from '../utils/_DATA';
+
+describe('questions reducer', () => {
+  const INITIAL_STATE = null;
+  const authedUser = 'sarahedo';
+  const qid = 'vthrdm985a262al8qx3do';
+  const answer = 'optionOne';
+
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual(INITIAL_STATE);
+  });
+
+  it('should handle RECEIVE_QUESTIONS', () => {
+    expect(
+      reducer(INITIAL_STATE, {
+        type: RECEIVE_QUESTIONS,
+        payload: questions,
+      })
+    ).toEqual(questions);
+  });
+
+  it('should handle SAVE_QUESTION_ANSWER_QUESTIONS', () => {
+    const currentState = questions;
+    expect(
+      reducer(currentState, {
+        type: SAVE_QUESTION_ANSWER_QUESTIONS,
+        payload: { authedUser, qid, answer },
+      })
+    ).toEqual({
+      ...currentState,
+      [qid]: {
+        ...currentState[qid],
+        [answer]: {
+          ...currentState[qid][answer],
+          votes: currentState[qid][answer].votes.concat([authedUser]),
+        },
+      },
+    });
+  });
+});
